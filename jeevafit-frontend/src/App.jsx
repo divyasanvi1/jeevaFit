4678132bb4963a66b4d9a1690f8b7ff62d2d0ae6
Ravi Kumar <rravikumar27316@gmail.com>
2025-04-23 14:40:57 +0530

    header done

--- jeevafit-frontend/src/App.jsx
+++ jeevafit-frontend/src/App.jsx
@@ -1,31 +1,14 @@
-// src/App.js
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignupPage';
-import HomePage from './pages/HomePage';
-import Dashboard from './pages/Dashboard';
-import NearestHospitalsPage from './pages/NearestHospitals';
-import './index.css'
-
-const App = () => {
+function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/nearest-hospitals" element={<NearestHospitalsPage />} />
-          {/* Add other routes for the dashboard or protected routes here */}
-        </Routes>
-      </Router>
-    </Provider>
+    <>
+      <Header />
+      <Outlet />
+    </>
   );
-};
+}
 
 export default App;
