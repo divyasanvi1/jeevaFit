139d1c93b9316ca4933ba3708cc573bf4b0bc367
divyasanvi1 <sanvidiv22@gmail.com>
2025-05-26 06:06:12 +0530

    authrization header token

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -5,6 +5,9 @@ async function restrictToLoggedInUserOnly(req,res,next) {
     try {
         const token = req.cookies.auth_token;
         //console.log("req:", req);
+        if (!token && req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+            token = req.headers.authorization.split(" ")[1];
+          }
         console.log("Incoming token:", token);
        console.log("Incoming Cookies:", req.cookies);
         //console.log("Incoming Headers:", req.headers);
